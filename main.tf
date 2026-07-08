f7bcb5278e3d239b5029c16d3182d0f8896001b2
tprietoa <t.prieto@duocuc.cl>
2026-07-08 12:10:36 -0400

    Se aplica terraform fmt para corregir formato

--- main.tf
+++ main.tf
@@ -10,8 +10,8 @@ terraform {
 
 # ---- VPC ----
 module "vpc" {
-  source   = "git::https://github.com/tprietoa/terraform-aws-vpc-AUY1105-TCPA.git?ref=v0.1.0"
-  
+  source = "git::https://github.com/tprietoa/terraform-aws-vpc-AUY1105-TCPA.git?ref=v0.1.0"
+
   vpc_cidr = var.vpc_cidr
   vpc_name = var.vpc_name
   # Las demás variables comentadas que tenías...
@@ -23,15 +23,15 @@ module "s3" {
 
   bucket_prefix = "tp-proyecto"
   bucket_suffix = "pro-2026"
-} 
+}
 
 
 # ---- EC2 ----
 module "ec2" {
-  source            = "git::https://github.com/tprietoa/terraform-aws-ec2-AUY1105-TCPA.git?ref=v0.1.0"
-  
-  subnet_id         = module.vpc.subnet_publica_1_id 
+  source = "git::https://github.com/tprietoa/terraform-aws-ec2-AUY1105-TCPA.git?ref=v0.1.0"
+
+  subnet_id         = module.vpc.subnet_publica_1_id
   vpc_id            = module.vpc.vpc_id
-  security_group_id = module.vpc.security_group_id   
+  security_group_id = module.vpc.security_group_id
   instance_name     = "MiInstancia"
 }
